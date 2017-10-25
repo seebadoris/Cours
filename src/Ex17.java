596b2db8168c715235cfe164ce45af13337a28bb
seebadoris <seebadoris666@gmail.com>
2017-10-25 16:53:03 +0200

    Add files via upload

--- src/Ex17.java
+++ src/Ex17.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
+
 
 public class Ex17 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String strNomConso;
+		int[] intNbrConso=new int [5];
+		int intConsoMax=-1;
+		Boolean blnQuitter=false;
+		
+		Scanner sc=new Scanner(System.in);
+		
+		do {
+			System.out.println(" Entrez le nom du consommable, Q pour Quitter");
+			strNomConso=sc.next();
+			if (strNomConso=="Q") {
+				blnQuitter=true;
+			}
+			
+			
+		}while (intNbrConso[intConsoMax]!=5|| blnQuitter==false);
+	
+	
 	}
+	
 
 }
